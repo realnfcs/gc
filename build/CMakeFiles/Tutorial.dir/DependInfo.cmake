73d0112ab2b60ebac4bb2c95c5ebf574c22af674
realnfcs <nicolas.cavalcante.dev@gmail.com>
2024-07-11 12:45:20 -0400

    first commit

---
+++ build/CMakeFiles/Tutorial.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/realnfcs/codes/gc/tutorial/libraries/include/glad/src/glad.c" "CMakeFiles/Tutorial.dir/libraries/include/glad/src/glad.c.o" "gcc" "CMakeFiles/Tutorial.dir/libraries/include/glad/src/glad.c.o.d"
+  "/home/realnfcs/codes/gc/tutorial/src/Camera.cpp" "CMakeFiles/Tutorial.dir/src/Camera.cpp.o" "gcc" "CMakeFiles/Tutorial.dir/src/Camera.cpp.o.d"
+  "/home/realnfcs/codes/gc/tutorial/src/EBO.cpp" "CMakeFiles/Tutorial.dir/src/EBO.cpp.o" "gcc" "CMakeFiles/Tutorial.dir/src/EBO.cpp.o.d"
+  "/home/realnfcs/codes/gc/tutorial/src/Mesh.cpp" "CMakeFiles/Tutorial.dir/src/Mesh.cpp.o" "gcc" "CMakeFiles/Tutorial.dir/src/Mesh.cpp.o.d"
+  "/home/realnfcs/codes/gc/tutorial/src/Model.cpp" "CMakeFiles/Tutorial.dir/src/Model.cpp.o" "gcc" "CMakeFiles/Tutorial.dir/src/Model.cpp.o.d"
+  "/home/realnfcs/codes/gc/tutorial/src/Texture.cpp" "CMakeFiles/Tutorial.dir/src/Texture.cpp.o" "gcc" "CMakeFiles/Tutorial.dir/src/Texture.cpp.o.d"
+  "/home/realnfcs/codes/gc/tutorial/src/VAO.cpp" "CMakeFiles/Tutorial.dir/src/VAO.cpp.o" "gcc" "CMakeFiles/Tutorial.dir/src/VAO.cpp.o.d"
+  "/home/realnfcs/codes/gc/tutorial/src/VBO.cpp" "CMakeFiles/Tutorial.dir/src/VBO.cpp.o" "gcc" "CMakeFiles/Tutorial.dir/src/VBO.cpp.o.d"
+  "/home/realnfcs/codes/gc/tutorial/src/main.cpp" "CMakeFiles/Tutorial.dir/src/main.cpp.o" "gcc" "CMakeFiles/Tutorial.dir/src/main.cpp.o.d"
+  "/home/realnfcs/codes/gc/tutorial/src/shaderClass.cpp" "CMakeFiles/Tutorial.dir/src/shaderClass.cpp.o" "gcc" "CMakeFiles/Tutorial.dir/src/shaderClass.cpp.o.d"
+  "/home/realnfcs/codes/gc/tutorial/src/stb.cpp" "CMakeFiles/Tutorial.dir/src/stb.cpp.o" "gcc" "CMakeFiles/Tutorial.dir/src/stb.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tutorial.dir/libraries/include/glad/src/glad.c.o"
+  "CMakeFiles/Tutorial.dir/libraries/include/glad/src/glad.c.o.d"
+  "CMakeFiles/Tutorial.dir/src/Camera.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/Camera.cpp.o.d"
+  "CMakeFiles/Tutorial.dir/src/EBO.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/EBO.cpp.o.d"
+  "CMakeFiles/Tutorial.dir/src/Mesh.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/Mesh.cpp.o.d"
+  "CMakeFiles/Tutorial.dir/src/Model.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/Model.cpp.o.d"
+  "CMakeFiles/Tutorial.dir/src/Texture.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/Texture.cpp.o.d"
+  "CMakeFiles/Tutorial.dir/src/VAO.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/VAO.cpp.o.d"
+  "CMakeFiles/Tutorial.dir/src/VBO.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/VBO.cpp.o.d"
+  "CMakeFiles/Tutorial.dir/src/main.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/main.cpp.o.d"
+  "CMakeFiles/Tutorial.dir/src/shaderClass.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/shaderClass.cpp.o.d"
+  "CMakeFiles/Tutorial.dir/src/stb.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/stb.cpp.o.d"
+  "Tutorial"
+  "Tutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
